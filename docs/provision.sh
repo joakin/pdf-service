7fc278e197b5cdc667915d818b01b6fd9b8b2adc
joakin <joaquin@chimeces.com>
2016-05-11 10:40:31 +0200

    add provision instruction for native modules

--- docs/provision.sh
+++ docs/provision.sh
@@ -1,5 +1,6 @@
 sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
 sudo npm install npm -g
 sudo npm install -g pm2
 
